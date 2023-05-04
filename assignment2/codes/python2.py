facb8c917b09058efa0f8669ac3aac95cc6e00a8
aryan <kumar.aryan.14022004@gmail.com>
2023-05-04 15:00:12 +0530

    second

--- assignment2/codes/python2.py
+++ assignment2/codes/python2.py
@@ -3,6 +3,9 @@ def main():
     assert n<=10
     #PXi_1 = 1/(10 - i)
     PofE = 1
+    for i in range(1,n+1,1):
+        print('PX_',i,'(1) = ', 1/(11 -i))
+
     for i in range(0,n,1):
         PofE = PofE*(1/(10 - i))
 
