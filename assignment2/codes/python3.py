1b63aa67e54b99f78411841e7a8f00f9a44813d0
aryan <kumar.aryan.14022004@gmail.com>
2023-05-06 08:50:47 +0530

    second

--- assignment2/codes/python3.py
+++ assignment2/codes/python3.py
@@ -6,8 +6,8 @@ combinations = list(itertools.permutations(range(10),4))
 # calculate the total number of combinations using len function to calculate length of list(combination).
 total_combinations = len(combinations)
 
-# count the number of combinations that open the suitcase letting correct sequence be (1, 2, 3, 4)
-correct_combinations = sum([1 for c in combinations if c == (1, 2, 3, 4)])
+# correct_combination = 1 as only one combination can open the lock. 
+correct_combinations = 1
 
 # calculating the probability of guessing the right sequence
 probability = correct_combinations / total_combinations
